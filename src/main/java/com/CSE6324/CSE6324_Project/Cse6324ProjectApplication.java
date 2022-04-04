46cd6e7d5dd5410380129965ce64f1b584fe09f7
Christian Teeples <36543358+cteeples@users.noreply.github.com>
2022-04-03 20:48:55 -0500

    first commit

---
+++ src/main/java/com/CSE6324/CSE6324_Project/Cse6324ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.CSE6324.CSE6324_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cse6324ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cse6324ProjectApplication.class, args);
+	}
+
+}
